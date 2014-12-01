further radius, fixed THINGS j calculation, plots

fit_THINGS_profiles.py
@@ -111,13 +111,15 @@ for filename in filenames:
 	plt.ylabel(r"Cumulative $M$")
 	
 	ax = fig.add_subplot(616)
-	ax.plot(radius, j_int, c='r', label='Int')
-	ax.plot(radius, j, c='k', label='Data, '+ratio_string)
+	ax.plot(radius, np.cumsum(J)/np.cumsum(M_integral)/get_model_j(radius, Rflat, Vmax), label='Data/Model')
+	#ax.plot(radius, get_model_j(radius, Rflat, Vmax), label="Model")#, c='k', label='Data, '+ratio_string)
 	ax.axvline(HLR, c='k', label="$R_{0.5}$")
 	ax.legend(loc='best')
 	plt.xlabel("radius, pc")
 	plt.ylabel(r"$j$")	
 	plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
+	plt.subplots_adjust(left=left, bottom=bottom, right=right, top=top, wspace=wspace, hspace=hspace)
+
 	plt.savefig("img/profiles_"+name, bbox_inches='tight')
 
 	#f = open('fit_values.csv', 'a')

mcmc.py
@@ -9,8 +9,10 @@ import sys
 import random
 
 prior = sys.argv[2]
+priorType = sys.argv[3]
 name = sys.argv[1]
-priorIncl = np.genfromtxt('chains/incl_'+name)
+#priorIncl = np.genfromtxt('chains/incl_'+name)
+GAMA_incl = get_GAMA_incl(name)
 
 
 
@@ -123,8 +125,17 @@ def lnProbAtan(pars, data):
    lnl = -0.5*((resid))
    return lnl
 
+
+def getTwoLevelPrior(pars, data):
+   name, x, y, vel, evel, r50, linewidth, HI_Vc_err = data
+   vmax, c, pa, incl, v0 = pars
+   incl_prior_sampling = np.random.choice(priorIncl)
+   LW = linewidth/(2*math.sin(incl_prior_sampling))
+   VmaxPrior = getVmaxPriorProb(vmax, LW, HI_Vc_err)
+   return VmaxPrior 
+
 def getPriorWidth(HI_Vc_err):
-  priorWidth =  10+HI_Vc_err
+  priorWidth =  5+HI_Vc_err
   return priorWidth
   
 def getProbDistribution(linewidth, HI_Vc_err, sigma):
@@ -132,6 +143,19 @@ def getProbDistribution(linewidth, HI_Vc_err, sigma):
     upper = linewidth + getPriorWidth(HI_Vc_err)
     probDist = stats.truncnorm((lower - linewidth) / sigma, (upper - linewidth) / sigma, loc=linewidth, scale=sigma)
     return probDist
+ 
+def getInclProbDistribution(incl, cutoff, sigma):
+    lower = linewidth - cutoff
+    upper = linewidth + cutoff
+    probDist = stats.truncnorm((lower - incl) / sigma, (upper - incl) / sigma, loc=incl, scale=sigma)
+    return probDist
+
+
+def getInclPrior(incl):
+    sigma=np.radians(3)   
+    probDist = getProbDistribution(GAMA_incl, np.radians(10), sigma)
+    priorProb = probDist.pdf(incl)   
+    return np.log(priorProb)
 
 def getVmaxPriorProb(vmax, LW, HI_Vc_err):
     sigma=getPriorWidth(HI_Vc_err)   
@@ -140,19 +164,12 @@ def getVmaxPriorProb(vmax, LW, HI_Vc_err):
     return np.log(priorProb)  
 
 def getLinewidthPrior(pars, data):
-   x, y, vel, evel, r50, linewidth, HI_Vc_err = data
-   vmax, c, pa, incl, v0 = pars
-   LW = linewidth/(2*math.sin(incl))
-   VmaxPrior = getVmaxPriorProb(vmax, LW, HI_Vc_err)
-   return VmaxPrior
-
-def getTwoLevelPrior(pars, data):
    name, x, y, vel, evel, r50, linewidth, HI_Vc_err = data
    vmax, c, pa, incl, v0 = pars
-   incl_prior_sampling = np.random.choice(priorIncl)
-   LW = linewidth/(2*math.sin(incl_prior_sampling))
-   VmaxPrior = getVmaxPriorProb(vmax, LW, HI_Vc_err)
-   return VmaxPrior   		
+   v22 = vmax*(1 - np.exp(-1*(200/(c*r50))))
+   LW = linewidth/(2*math.sin(GAMA_incl))
+   VmaxPrior = getVmaxPriorProb(v22, LW, HI_Vc_err)
+   return VmaxPrior
 
 
 def lnProbExp(pars, data):
@@ -163,7 +180,11 @@ def lnProbExp(pars, data):
    resid = np.sum((vel - m_vel)**2/(vel_err)**2)
    lnl = -0.5*((resid))
    if prior =='True':
-		linewidthPrior=getTwoLevelPrior(pars, data)#getLinewidthPrior(pars, data)
-		return lnl + linewidthPrior
+		if priorType == '2':
+			dataPrior = getInclPrior(incl)
+		elif priorType == '3':
+			dataPrior = getInclPrior(incl) + getLinewidthPrior(pars, data)
+		return lnl  + dataPrior
    else:
 		return lnl
+

read_SAMI_velfields.py
@@ -23,16 +23,9 @@ Nthreads = 4 # number of threads to run
 fit = 'exp'
 test=False
 prior = sys.argv[2]
+priorType = sys.argv[3]
 
-
-left  = 0.125  # the left side of the subplots of the figure
-right = 0.9    # the right side of the subplots of the figure
-bottom = 0.2   # the bottom of the subplots of the figure
-top = 0.9      # the top of the subplots of the figure
-wspace = 0.2   # the amount of width reserved for blank space between subplots
-hspace = 0.5   # the amount of height reserved for white space between subplots	
-
-
+GAMA_incl = get_GAMA_incl(name)
 
 
 def plotPDF(params, names, filename):
@@ -87,11 +80,11 @@ def fit_galaxy(name, x, y, vel, vel_err, r50, HI_linewidth, HI_Vc_err, initParam
 
 	elif fit == 'exp':
 		if prior =='True':
-			PDFfilename = 'img/pdf/2_prior_'+name
-			mcmcOutFile = '2_prior_mcmc.csv'
-			mcmcImgName = 'img/models/good/2_prior_'+fit+'_RC_'+name
-			histName = 'img/models/hist/2_prior_incl_'+name
-			incl_chainFilename = 'chains/2_prior_incl_'+name
+			PDFfilename = 'img/pdf/'+priorType+'_prior_'+name
+			mcmcOutFile = priorType+'_prior_mcmc.csv'
+			mcmcImgName = 'img/models/good/'+priorType+'_prior_'+fit+'_RC_'+name
+			histName = 'img/models/hist/'+priorType+'_prior_incl_'+name
+			incl_chainFilename = 'chains/'+priorType+'_prior_incl_'+name
 		else:
 			PDFfilename = 'img/pdf/'+name
 			mcmcOutFile = 'mcmc.csv'
@@ -117,7 +110,7 @@ def fit_galaxy(name, x, y, vel, vel_err, r50, HI_linewidth, HI_Vc_err, initParam
 		model_radius = np.linspace(0, 25, 1000)
 		model_RC = vc_mod*(1 - np.exp(-1*(model_radius/(c_mod*r50)))) #((2/math.pi) * vc_mod* np.tanh(model_radius/(c_mod*r50))) 
 		model_linewidth = HI_linewidth/(2*np.sin(incl_mod))
-
+		modelV_rot = vc_mod*(1 - np.exp(-1*(80/(c_mod*r50))))
 		
 	delta_coords = get_delta_coords(name)
 	delta_z = get_delta_z(name)
@@ -131,7 +124,7 @@ def fit_galaxy(name, x, y, vel, vel_err, r50, HI_linewidth, HI_Vc_err, initParam
 	c = plt.scatter(x, y, c=vel, marker='s', edgecolor='none', vmin=-150, vmax=150)
 	plt.colorbar(c, label='vel, km/s')
 	fig.add_subplot(222)
-	
+	plt.title(' Incl:' +str(round(np.degrees(incl_mod), 1))+' GAMA incl:' +str(round(np.degrees(GAMA_incl), 1)))
 	c = plt.scatter(model_radius, np.abs(model_RC), c='k', s=4, edgecolor='none', label='Model RC', zorder=100)
 	d = plt.scatter(fit_radius, np.abs(fit_vel), c=np.abs(vel), s=5, edgecolor='none', label='Fit RC')
 	plt.axhline(model_linewidth, c='r', linewidth=2, linestyle='dotted', label='W50/(2sin(i))')	
@@ -143,7 +136,7 @@ def fit_galaxy(name, x, y, vel, vel_err, r50, HI_linewidth, HI_Vc_err, initParam
 	plt.colorbar(c, label='vel, km/s')
 	plt.axhline(c='k')
 	plt.axvline(c='k')
-	plt.title('Vmax:' + str(np.round(vc_mod, 1))+'HI linewidth: '+str(round(model_linewidth))+' Incl:' +str(round(np.degrees(incl_mod), 1))+' W50:'+str(round(HI_linewidth/2, 0)))
+	plt.title('Vc:' + str(np.round(modelV_rot, 1))+'HI linewidth: '+str(round(model_linewidth))+' W50:'+str(round(HI_linewidth/2, 0)))
 	fig.add_subplot(224)
 	c = plt.scatter(x, y, c= vel - modelVelField, edgecolor='none', marker='s', vmin=-150, vmax=150)
 	plt.colorbar(c, label='Residuals, km/s')

test_fit.py
@@ -9,6 +9,7 @@ from scipy.odr import Data, Model, ODR, RealData, odr_stop
 from nicer_plots import *
 
 
+
 filenames = np.genfromtxt("Simona/THINGS_files.txt", dtype='object')
 J_HLR = []
 for filename in filenames:
@@ -27,12 +28,11 @@ for filename in filenames:
 	print hlr, 'hlr', np.round(np.cumsum(Sigma)/np.max(np.cumsum(Sigma)), 1)
 	HLR = radius[hlr]
 	#removing velocity range above 1 HLR:
-	
-	vel2 = np.ma.masked_where((radius > HLR) & (radius < radius[-1]) , vel)
-	print vel.shape
-	vel_err2 = np.ma.masked_where((radius > HLR) & (radius < radius[-1]) , vel_err)
-	radius2 = np.ma.masked_where((radius > HLR) & (radius < radius[-1]) , radius)
-	print radius2
+	test_radius = radius[-11]
+	print test_radius, 'test_radius'
+	vel2 = np.ma.masked_where((radius > HLR) & (radius <> test_radius) , vel)
+	vel_err2 = np.ma.masked_where((radius > HLR) & (radius <> test_radius) , vel_err)
+	radius2 = np.ma.masked_where((radius > HLR) & (radius <> test_radius) , radius)
 	vel_err2[-1] = 40
 	#vel2[-1] = Vmax
 
@@ -69,29 +69,36 @@ for filename in filenames:
 	M_integral = get_M_integral(radius, S1, S2, R1, R2)
 	j_int = get_j_int(J_int, M_integral)
 	
-	fig = plt.figure(figsize=(12, 10))
+	fig = plt.figure(figsize=(figsize))
 	ratio_string = str(np.round(R2/Rflat, 2))+r" $R_{s2}/R_{flat}$"
-	if stopInt <> 1:
-		plt.title(stopReason[0], color='r')
-	else:
-		plt.title(stopReason[0], color='k', fontsize=11)
+	#if stopInt <> 1:
+		#plt.title(stopReason[0], color='r')
+	plt.title("NGC2403, THINGS HI data")
+	#else:
+	#	plt.title(stopReason[0], color='k', fontsize=11)
 	ax = fig.add_subplot(611)
-	ax.plot(radius, RC_model([Vmax, Rflat], radius), c='r', label='Fit')
+	ax.plot(radius, RC_model([Vmax, Rflat], radius), c='r', label='Model')
+	ax.errorbar(radius2, vel2, yerr=vel_err2, c='k', linestyle='none', label="Fit velocity data")
+
 	#ax.errorbar(radius[radius < hlr], vel[radius < hlr], yerr=vel_err[radius < hlr], c='k', linestyle='none')
 	ax.axvline(Rflat, c='r', label="$R_{flat}$")
 	ax.axvline(HLR, c='k', label="$R_{0.5}$")
-	ax.plot(radius, vel, c='k')
+	ax.plot(radius, vel, c='k', alpha=0.4, label="")
 	ax.axvspan(0, HLR, facecolor='g', alpha=0.4)
-	ax.axvspan(np.max(radius)-1, np.max(radius), facecolor='g', alpha=0.4)
+	ax.axvspan(test_radius-1, test_radius, facecolor='g', alpha=1)
 	ax.axis([0, radius[-1]+300, 0, np.max(vel)+20])
+	ax.set_xlim([0,22000])
+	ax.legend(loc='best')
 	plt.xlabel("radius, pc")
-	plt.ylabel("velocity, km/s")
+	plt.ylabel("Velocity, km/s")
 
 	ax = fig.add_subplot(612)
 	ax.errorbar(radius, Sigma, yerr=Sigma_err, c='k', linestyle='none')
-	ax.plot(radius, Sigma, c='k')
-	ax.plot(radius, Sigma_model_2exp([S1, S2, R1, R2], radius), c='r', label='Fit')
+	ax.plot(radius, Sigma, c='k', label="Data")
+	ax.plot(radius, Sigma_model_2exp([S1, S2, R1, R2], radius), c='r', label='Double-exponential fit')
 	ax.axvline(HLR, c='k', label="$R_{0.5}$")
+	ax.legend(loc='best')
+	ax.set_xlim([0,22000])
 	plt.xlabel("radius, pc")
 	plt.ylabel(r"$\Sigma, M_{\odot}/pc^2$")
 	plt.yscale('log')
@@ -103,6 +110,7 @@ for filename in filenames:
 	ax.plot(radius, J, c='k', label='Data')
 	ax.axvline(HLR, c='k', label="$R_{0.5}$")
 	ax.legend(loc='best')
+	ax.set_xlim([0,22000])
 	plt.xlabel("radius, pc")
 	plt.ylabel(r" $J_{ring}$")
 	
@@ -112,6 +120,7 @@ for filename in filenames:
 	ax.plot(radius, np.cumsum(J_int), c='r', label='Int')
 	ax.axvline(HLR, c='k', label="$R_{0.5}$")
 	ax.legend(loc='best')
+	ax.set_xlim([0,22000])
 	plt.xlabel("radius, pc")
 	plt.ylabel(r"Cumulative $J$")
 	
@@ -119,17 +128,22 @@ for filename in filenames:
 	ax.plot(radius, np.cumsum(M_integral), c='r', label='Int')
 	ax.plot(radius, np.cumsum(ring_mass), c='k', label='Data')
 	ax.axvline(HLR, c='k', label="$R_{0.5}$")
+	ax.set_xlim([0,22000])
 	ax.legend(loc='best')
 	plt.xlabel("radius, pc")
 	plt.ylabel(r"Cumulative $M$")
 	
 	ax = fig.add_subplot(616)
-	ax.plot(radius, np.cumsum(j_int), c='r', label='Int')
-	ax.plot(radius, np.cumsum(j), c='k', label='Data, '+ratio_string)
-	ax.axvline(HLR, c='k', label="$R_{0.5}$= "+str(round(HLR/1000, 1)))
+	#ax.plot(radius, np.cumsum(j_int), c='r', label='Int')
+	#ax.plot(radius, np.cumsum(j), c='k', label='Data, '+ratio_string)
+	ax.plot(radius, get_model_j(radius, Rflat, Vmax)/(np.cumsum(J)/np.cumsum(M_integral)), c='k', label='Model/Data')
+	ax.axvline(HLR, c='k', label="$R_{0.5}$= "+str(round(HLR/1000, 1))+" kpc")
+	ax.axvspan(0, HLR, facecolor='g', alpha=0.4)
+	ax.axhline(1, c='k', linestyle='dotted')
 	ax.legend(loc='best')
 	plt.xlabel("radius, pc")
-	plt.ylabel(r"Cumulative $j$")	
+	plt.ylabel(r"$j$")
+	ax.set_xlim([0,22000])
 	plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
 	plt.savefig("img/test_"+name, bbox_inches='tight')
 